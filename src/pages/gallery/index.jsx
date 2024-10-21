71c8dac522444c1079e31ad74c928fe87077fcff
Raja <raja.rathinam@pcisage.com>
2024-10-21 12:47:42 +0530

    dynamic gallery

--- src/pages/gallery/index.jsx
+++ src/pages/gallery/index.jsx
@@ -1,8 +1,29 @@
 import GalleryScreen from "@/components/screens/gallery/gallery";
+import { getAllData } from "@/libs/firebase/firebase";
 import React from "react";
 
-const GalleryPage = () => {
-  return <GalleryScreen />;
+const GalleryPage = ({ images }) => {
+
+  return <GalleryScreen images={images} />;
 };
 
 export default GalleryPage;
+
+export const getServerSideProps = async () => {
+  try {
+    const res = await getAllData("gallery_images");
+
+    return {
+      props: {
+        images: res,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        images: [],
+        error: error.message,
+      },
+    };
+  }
+};
